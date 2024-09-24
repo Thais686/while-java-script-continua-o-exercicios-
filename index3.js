8dd225fdd9fb0c6a0128675018b8a1d7e62b3eab
Thaís <thaiscastro95@yahoo.com>
2024-09-24 08:06:42 -0300

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,11 @@
+var soma=0
+do{
+
+    var numero= Number (prompt("Digite um numero "))
+
+     soma= numero+ soma
+}
+while(numero!=0)
+
+alert(soma)
+
---
+++ index4.js
@@ -0,0 +1,17 @@
+let numero
+let n=1
+let fatorial=1
+
+numero= Number (prompt ("Digite um numero"))
+
+
+while(n <= numero){
+
+
+    fatorial= fatorial* n
+    n++
+}
+
+
+
+alert(fatorial)
